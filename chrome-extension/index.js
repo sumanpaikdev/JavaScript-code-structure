ed597c731f1a8383903c58a659b2ce5cc8c01b25
Suman Paik <sumanpaik.dev@gmail.com>
2022-10-19 18:10:58 +0530

    chrome extention manifest_version added

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,12 +1,20 @@
-let myContainer = ["www.goolge.com", "www.facebook.com", "www.twitter.com", "www.showwcase.com"]
+let myContainer = []
 const inputEl = document.getElementById("input-element")
 const saveEl = document.getElementById("save-btn")
-let ulEl = document.getElementById("ul-element")
+const ulEl = document.getElementById("ul-element")
 
-saveEl.addEventListener("click", function() {
-    myContainer.push(inputEl.value) 
+saveEl.addEventListener("click", function () {
+    myContainer.push(inputEl.value)
+    connectInner()
+    inputEl.value = ""
     console.log(myContainer)
 })
-for(let i = 0; i < myContainer.length; i++) {
-    ulEl.innerHTML += "<li>" +myContainer[i] + "</li>"
+
+
+function connectInner() {
+    let innerItems = ""
+    for (let i = 0; i < myContainer.length; i++) {
+        innerItems += `<li><a href='${myContainer[i]}'>${myContainer[i]}</a></li>`
+    }
+    ulEl.innerHTML = innerItems
 }
